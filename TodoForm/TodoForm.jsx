cb90fa953a409afa2cf63dac5bced434d98dc10b
진서현 <jin760223@gmail.com>
2026-02-06 23:25:34 +0900

    입력 값 검증 기능 완성

--- TodoForm/TodoForm.jsx
+++ TodoForm/TodoForm.jsx
@@ -3,18 +3,26 @@ import { useState } from 'react';
 function TodoForm({ onAdd }) {
   const [input, setInput] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault(); // submit은 페이지 새로고침 되어버리면 아직 localStorage 설정 안되 있어
-    onAdd(input);
-    setInput(''); // 입력창 초기화
+  const handleSubmit = (inputTodo) => {
+    inputTodo.preventDefault();
+    try {
+      if (!input.trim()) throw new Error('내용을 입력해주세요');
+
+      onAdd(input);
+      setInput(''); // 입력창 초기화
+    } catch (error) {
+      console.error(error.message);
+    }
   };
 
   return (
+    // trim으로 인해서 아무 값을 입력하지 않으면 false가 됨
     <form onSubmit={handleSubmit}>
       <input
         value={input}
         onChange={(e) => setInput(e.target.value)}
         placeholder="할일 입력"
+        className="input-todo"
       />
     </form>
   );
--- TodoList/TodoItem/TodoList.jsx
+++ TodoList/TodoItem/TodoList.jsx
@@ -1,10 +1,7 @@
 import { useState } from 'react';
 
 function TodoList({ items, onToggleTodo, delItems }) {
-  // const handleCheck = (todoId) => {
-  //   onToggleTodo(todoId);
-  // };
-
+  // 핸들러 함수 선언 없이 이벤트 속성에서 바로 부모 컴포넌트로 반환
   return items.map((item) => {
     return (
       <div className="todo-card" key={item.id}>
