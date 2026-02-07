3f6ca681d45f0864ef627dd63b3e1eb571781d64
진서현 <jin760223@gmail.com>
2026-02-07 16:47:12 +0900

    github 배포 설정 수정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ReactTodolist/', // 이 부분이 없으면 빌드 후에도 파일을 못 찾습니다!
   plugins: [react()],
-})
+});
